4d1b5a6ad26df6e19b39f857ddd91b7cf3e8f47a
khareshekher@gmail.com <khareshekher@gmail.com>
2018-05-03 01:13:26 +0530

    blank project commit

---
+++ Yoga-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Yoga_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
